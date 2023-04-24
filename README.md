d8fa4190c1820081ed0c5ce5d1c6fde6d8388766
panxiaochao <panxiaochao602@qq.com>
2023-04-24 16:07:39 +0800

    optimized code 正式发布版本 2022-2.7-1.3 2022-2022.0.6-1.3

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ pxc-framework Converged Project, base on spring boot 2.7.X up-to-date
 
 ### current version
 
-- Spring Boot 2.7.10
-- Spring Cloud 2022.0.5
+- Spring Boot 2.7.11
+- Spring Cloud 2022.0.6
 - Spring Cloud Alibaba 2021.0.5.0
 - jdk >= 1.8
--- pxc-spring-boot-demo/src/main/java/io/github/panxiaochao/TestController.java
+++ pxc-spring-boot-demo/src/main/java/io/github/panxiaochao/TestController.java
@@ -1,8 +1,10 @@
 package io.github.panxiaochao;
 
 import io.github.panxiaochao.common.response.R;
+import io.github.panxiaochao.database.metadata.builder.DefaultRulesBuilder;
+import io.github.panxiaochao.database.metadata.po.ITable;
+import io.github.panxiaochao.database.metadata.tool.FastDbTool;
 import io.github.panxiaochao.file.storage.engine.FileStorageEngine;
-import io.github.panxiaochao.file.storage.meta.FileMetadata;
 import io.github.panxiaochao.threadpool.executor.ThreadPoolTaskManager;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -14,12 +16,12 @@ import lombok.Setter;
 import lombok.ToString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.MediaType;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -56,6 +58,32 @@ public class TestController {
         return user;
     }
 
+
+    @GetMapping(path = "/table")
+    @Operation(summary = "获取table", description = "获取schema", method = "GET")
+    @Parameter(name = "tableName", description = "表名")
+    @Parameter(name = "fillColumns", description = "是否加载数据库表字段")
+    public R<List<ITable>> table(
+            @RequestParam(required = false) String tableName,
+            @RequestParam(required = false) boolean fillColumns
+    ) {
+        FastDbTool fastDbTool = FastDbTool.create(
+                "jdbc:mysql://localhost:3306/oauth2?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai",
+                "root",
+                "root123456"
+        );
+        if (fillColumns) {
+            fastDbTool.rulesBuilder(DefaultRulesBuilder.Builder::enableFillColumns);
+        }
+        if (StringUtils.hasText(tableName)) {
+            ITable table = fastDbTool.queryTables(tableName);
+            return R.ok(Collections.singletonList(table));
+        } else {
+            List<ITable> tables = fastDbTool.queryTables();
+            return R.ok(tables);
+        }
+    }
+
     /**
      * 测试接口
      *
@@ -67,46 +95,47 @@ public class TestController {
         return user;
     }
 
-    @Operation(summary = "上传文件接口", description = "上传文件接口描述", method = "POST")
-    @PostMapping(path = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public R<FileMetadata> upload(
-            @Parameter(description = "要上传的文件", required = true) @RequestPart("file") MultipartFile file) {
-        FileMetadata fileMetadata = fileStorageEngine.upload(file);
-        return R.ok(fileMetadata);
-    }
+    // @Operation(summary = "上传文件接口", description = "上传文件接口描述", method = "POST")
+    // @PostMapping(path = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    // public R<FileMetadata> upload(
+    //         @Parameter(description = "要上传的文件", required = true) @RequestPart("file") MultipartFile file) {
+    //     FileMetadata fileMetadata = fileStorageEngine.upload(file);
+    //     return R.ok(fileMetadata);
+    // }
+
 
     @Getter
     @Setter
     @ToString
-    @Schema(name = "用户信息", description = "用户信息")
+    @Schema(name = "用户信息", description = "用户信息描述")
     public static class User {
-        @Schema(name = "userName", description = "用户名", title = "sss")
+        @Schema(description = "用户名")
         private String userName;
-        @Schema(name = "intId")
+        @Schema(description = "int-ID")
         private int intId;
-        @Schema(name = "integerId")
+        @Schema(description = "Integer-ID")
         private Integer integerId;
-        @Schema(name = "longId")
+        @Schema(description = "long-ID")
         private long longId;
-        @Schema(name = "pLongId")
+        @Schema(description = "Long-ID")
         private Long pLongId;
-        @Schema(name = "floatId")
+        @Schema(description = "float-ID")
         private float floatId;
-        @Schema(name = "pFloatId")
+        @Schema(description = "pFloatId-ID")
         private Float pFloatId;
-        @Schema(name = "doubleId")
+        @Schema(description = "double-ID")
         private double doubleId;
-        @Schema(name = "pDoubleId")
+        @Schema(description = "Double-ID")
         private Double pDoubleId;
-        @Schema(name = "bigDecimalId")
+        @Schema(description = "BigDecimal-ID")
         private BigDecimal bigDecimalId;
-        @Schema(name = "map")
+        @Schema(description = "Map对象")
         private Map<String, String> map;
-        @Schema(name = "list")
+        @Schema(description = "List用户列表")
         private List<User> list;
-        @Schema(name = "createDate")
+        @Schema(description = "Date时间")
         private Date createDate;
-        @Schema(name = "createDateTime")
+        @Schema(description = "LocalDateTime时间")
         private LocalDateTime createDateTime;
     }
 }
