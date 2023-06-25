9cabf4f5eeb9e9b9c2af11aedec04f979f0a2807
panxiaochao <panxiaochao602@qq.com>
2023-06-25 10:48:27 +0800

    optimized code 正式发布版本 2022-2.7-1.4 2022-2022.0.7-1.4

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ pxc-framework Converged Project, base on spring boot 2.7.X up-to-date
 
 ### current version
 
-- Spring Boot 2.7.11
+- Spring Boot 2.7.12
 - Spring Cloud 2022.0.6
 - Spring Cloud Alibaba 2021.0.5.0
 - jdk >= 1.8
--- pxc-spring-boot-demo/src/main/java/io/github/test/Application.java
+++ pxc-spring-boot-demo/src/main/java/io/github/test/Application.java
@@ -1,9 +1,10 @@
-package io.github.panxiaochao;
+package io.github.test;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -13,12 +14,13 @@ import org.springframework.util.StringUtils;
 import java.net.InetAddress;
 
 /**
+ * <p>Multiple Spring Data modules found, entering strict repository configuration mode. exclude RedisRepositoriesAutoConfiguration.class</p>
+ *
  * @author Lypxc
  */
-@SpringBootApplication(
-        exclude = {DataSourceAutoConfiguration.class, JdbcTemplateAutoConfiguration.class}
-)
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, JdbcTemplateAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class})
 public class Application {
+
     private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
     /**
@@ -37,9 +39,9 @@ public class Application {
         }
 
         LOG.info("\n----------------------------------------------------------\n\t{}{}{}{}",
-                applicationName + " is running! Access URLs:",
-                "\n\tLocal    访问网址: \thttp://localhost:" + port + path,
+                applicationName + " is running! Access URLs:", "\n\tLocal    访问网址: \thttp://localhost:" + port + path,
                 "\n\tExternal 访问网址: \thttp://" + ip + ":" + port + path,
                 "\n----------------------------------------------------------\n");
     }
+
 }
